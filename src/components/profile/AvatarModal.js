0409150591166a0b604d67d569a59f706687b4f8
shmohamud <shmohamud@email.wm.edu>
2019-11-11 16:24:22 -0500

    begin styling, classnames boilerplate, colored borders added to components

--- src/components/profile/AvatarModal.js
+++ src/components/profile/AvatarModal.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import classNames from 'classnames'
 const AvatarModal = props => {
+
+  let avatarClasses = classNames({
+    'avatar-img':true
+  })
+
   const { avatar } = props;
   const defaultAvatar = "https://cdn.staticneo.com/w/avatar/thumb/c/ce/Aang.png/200px-Aang.png";
   let src = avatar ? avatar : defaultAvatar;
   return (
-    <div>
+    <div className={avatarClasses}>
       <img src={src} />
     </div>
   );
--- src/components/profile/Header.js
+++ src/components/profile/Header.js
@@ -1,14 +1,19 @@
 import React from "react";
+import classNames from 'classnames'
 import HeaderContent from "./HeaderContent";
 import AvatarModal from "./AvatarModal";
 import useAuth from '../../hooks/useAuth'
 
 
+let headerClasses = classNames({
+  'header':true
+})
+
 const Header = () => {
   const {me:{profilePicture}} = useAuth()
 
   return (
-    <div>
+    <div className={headerClasses}>
       <AvatarModal avatar={profilePicture}/>
       <HeaderContent />
     </div>
