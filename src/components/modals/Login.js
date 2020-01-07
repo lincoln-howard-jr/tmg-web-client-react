f2cc22d74993d20b7f43d98396f4829fab4f4549
Lincoln Howard <lincoln.c.howard.jr@gmail.com>
2020-01-07 10:13:57 -0500

    css fixes

--- src/components/modals/Login.js
+++ src/components/modals/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useApp} from "../../AppProvider";
+import HeaderImage from '../profile/header.png'
 
 function Login({onDone}) {
   const { useForm, useAuth: {login} } = useApp ();
@@ -18,15 +19,13 @@ function Login({onDone}) {
   const { err, handleChange, handleSubmit } = useForm(onSubmit);
   return (
     <>
+      <img src={HeaderImage} />
+      <h2 className="translate-y-minus-25">Log In</h2>
       <span>{err && err.toString ()}</span>
       <form onSubmit={handleSubmit}>
-        <label>Username: </label>
-        <input type="text" name="username" onChange={handleChange} />
-        <br />
-        <label>Password: </label>
-        <input type="text" name="password" onChange={handleChange} />
-        <br />
-        <input type="submit" value="Submit" />
+        <input type="text" name="username" placeholder="Username" onChange={handleChange} />
+        <input type="text" name="password" placeholder="Password" onChange={handleChange} />
+        <button type="submit">Submit</button>
       </form>
     </>
   );
